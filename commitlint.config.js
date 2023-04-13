fae65c08f37cd2b75b0436a103b231cca0d02541
kleva-j <kasmickleva@gmail.com>
2023-04-13 02:35:48 +0100

    chore: initialize nextjs app with trpc

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "chore",
+        "fix",
+        "doc",
+        "style",
+        "refactor",
+        "test",
+        "revert",
+        "build",
+        "ci",
+        "perf",
+        "conf",
+      ],
+    ],
+  },
+};
