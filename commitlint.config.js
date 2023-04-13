30e8536649dcf158c2e311ea14a60ba5254476f5
kleva-j <kasmickleva@gmail.com>
2023-04-13 10:28:12 +0100

    chore: setup editor config and reformat all documents

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [
-      2,
-      "always",
-      [
-        "feat",
-        "chore",
-        "fix",
-        "doc",
-        "style",
-        "refactor",
-        "test",
-        "revert",
-        "build",
-        "ci",
-        "perf",
-        "conf",
-      ],
-    ],
-  },
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"feat",
+				"chore",
+				"fix",
+				"doc",
+				"style",
+				"refactor",
+				"test",
+				"revert",
+				"build",
+				"ci",
+				"perf",
+				"conf",
+			],
+		],
+	},
 };
