66a17a02f19514d072199d2f15772cde63192a33
kleva-j <kasmickleva@gmail.com>
2023-04-15 23:50:26 +0100

    refactor: add minor changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,13 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+	if (request.nextUrl.pathname.startsWith("/dashboard")) {
+		return NextResponse.rewrite(new URL("/dashboard/user", request.url));
+	}
+}
+
 // Stop Middleware running on static files
 export const config = {
 	matcher: [
