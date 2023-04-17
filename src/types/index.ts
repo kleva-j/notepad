0bf882de3d0871dd7d9e761a29fe76e20eb5e064
kleva-j <kasmickleva@gmail.com>
2023-04-18 00:01:28 +0100

    feat: update app types and enums to be used in the app

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { Folder } from "@/utils/enums";
+
 export interface CategoryItem {
 	id: string;
 	name: string;
@@ -25,4 +27,43 @@ export interface AuthState {
 	currentUser: GithubUser;
 	isAuthenticated: boolean;
 	error?: string;
+}
+
+export interface CategoryState {
+	categories: CategoryItem[];
+	error: string;
+	loading: boolean;
+}
+
+export interface NoteState {
+	notes: NoteItem[];
+	activeFolder: Folder;
+	activeNoteId: string;
+	selectedNotesIds: string[];
+	activeCategoryId: string;
+	error: string;
+	loading: boolean;
+	searchValue: string;
+}
+
+export interface SettingsState {
+	isOpen: boolean;
+	loading: boolean;
+	darkTheme: boolean;
+	sidebarVisible: boolean;
+}
+
+export interface SyncState {
+	syncing: boolean;
+	lastSynced: string;
+	error: string;
+	pendingSync: boolean;
+}
+
+export interface RootState {
+	authState: AuthState;
+	categoryState: CategoryState;
+	noteState: NoteState;
+	settingsState: SettingsState;
+	syncState: SyncState;
 }
