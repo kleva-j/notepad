ea5668062114c30e20d488e72802c9f5ad9cf6a6
kleva-j <kasmickleva@gmail.com>
2023-04-17 23:59:24 +0100

    feat: setup basic login and signup pages

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoginPage() {
+	return (
+		<div className="container flex h-screen w-screen flex-col items-center justify-center">
+			This is the Login page.
+		</div>
+	);
+}
---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SignupPage() {
+	return (
+		<div className="container flex h-screen w-screen flex-col items-center justify-center">
+			This is the Signup page.
+		</div>
+	);
+}
