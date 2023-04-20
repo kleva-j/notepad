baeba3a35b19a9d2eb535e3a989fe28c258c3ac7
kleva-j <kasmickleva@gmail.com>
2023-04-21 00:42:05 +0100

    feat: add links to fetch google fonts (roboto + lato)

--- src/component/SeoHeader.tsx
+++ src/component/SeoHeader.tsx
@@ -11,6 +11,21 @@ export const SeoHeader = () => (
 	<Head>
 		<title>{metas.title}</title>
 
+		<link
+			rel="preconnect"
+			href="https://fonts.gstatic.com"
+			crossOrigin="anonymous"
+		/>
+		<link
+			href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300&display=swap"
+			rel="stylesheet"
+		/>
+
+		<link
+			href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+			rel="stylesheet"
+		></link>
+
 		<meta property="og:title" content={metas.title} key="og:title" />
 		<meta property="og:image" content={metas.image} key="og:image" />
 		<meta
