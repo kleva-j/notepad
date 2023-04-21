7fd8b215d20e74963176ebf20f9e31eb8998dfd4
kleva-j <kasmickleva@gmail.com>
2023-04-21 01:07:04 +0100

    feat: set initial auth session state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,21 @@
-import { SeoHeader } from "@/component/SeoHeader";
-
 import React, { PropsWithChildren } from "react";
+
+import { SeoHeader } from "@/component/SeoHeader";
+import { getSession } from "@/lib/session";
+import { headers } from "next/headers";
 
 import Providers from "./provider";
 
 import "@/styles/globals.css";
 
-function RootLayout({ children }: PropsWithChildren) {
+async function RootLayout({ children }: PropsWithChildren) {
+	const session = await getSession(headers().get("cookie") ?? "");
+
 	return (
 		<html lang="en">
 			<SeoHeader />
 			<body>
-				<Providers>{children}</Providers>
+				<Providers session={session}>{children}</Providers>
 			</body>
 		</html>
 	);
