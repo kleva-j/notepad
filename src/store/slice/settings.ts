b71614471b3f6f983aa8fefd6a8d8c29c433c5d4
kleva-j <kasmickleva@gmail.com>
2023-05-05 21:17:08 +0100

    feat: modify settings state and save to storage

--- src/store/slice/settings.ts
+++ src/store/slice/settings.ts
@@ -1,4 +1,4 @@
-import { atomWithLocalStorage } from "..";
+import { atomWithStorage } from "jotai/utils";
 import { SettingsState } from "@/types";
 
 const initialState: SettingsState = {
@@ -6,16 +6,24 @@ const initialState: SettingsState = {
 	loading: false,
 	darkTheme: false,
 	sidebarVisible: false,
+	previewMarkdown: false,
 };
 
 export const updateSettings = (
 	state: SettingsState,
-	{ isOpen, loading, darkTheme, sidebarVisible }: Partial<SettingsState>
+	{
+		isOpen,
+		loading,
+		darkTheme,
+		sidebarVisible,
+		previewMarkdown,
+	}: Partial<SettingsState>
 ): SettingsState => ({
 	isOpen: isOpen || state.isOpen,
 	loading: loading || state.loading,
 	darkTheme: darkTheme || state.darkTheme,
 	sidebarVisible: sidebarVisible || state.sidebarVisible,
+	previewMarkdown: previewMarkdown || state.previewMarkdown,
 });
 
-export const SettingsStateAtom = atomWithLocalStorage("settings", initialState);
+export const SettingsStateAtom = atomWithStorage("settings", initialState);
