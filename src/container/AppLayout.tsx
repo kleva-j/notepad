3a8281ef5db23c2a3796b436e7725177393cfef4
kleva-j <kasmickleva@gmail.com>
2023-05-05 21:16:11 +0100

    feat: setup dragdrop context and implement swap category feature

--- src/container/AppLayout.tsx
+++ src/container/AppLayout.tsx
@@ -5,27 +5,58 @@ import NoteEditor from "./AppEditor";
 import NoteList from "./AppNoteList";
 import React from "react";
 
+import {
+	swapCategories,
+	CategoryStateAtom,
+	updateCategoryStateAtom,
+} from "@/store/slice/category";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { fadeIn } from "@/utils/motion";
 import { motion } from "framer-motion";
 import { Allotment } from "allotment";
+import { useAtom } from "jotai";
 
 import "allotment/dist/style.css";
 
 export const Layout = () => {
+	const [{ categories }] = useAtom(CategoryStateAtom);
+	const [, updateCategory] = useAtom(updateCategoryStateAtom);
+
+	const onDragEnd = (result: DropResult) => {
+		const { destination, source } = result;
+		if (!destination) return;
+		if (
+			destination.droppableId === source.droppableId &&
+			destination.index === source.index
+		)
+			return;
+
+		if (result.type === "CATEGORY") {
+			updateCategory({
+				categories: swapCategories(categories, {
+					sourceId: source.index,
+					destinationId: destination.index,
+				}),
+			});
+		}
+	};
+
 	return (
 		<motion.section
 			variants={fadeIn("right", "spring", 0.5, 0.75)}
 			className="flex h-screen w-screen"
 		>
-			<Allotment minSize={200} proportionalLayout>
-				<Allotment.Pane preferredSize={240} maxSize={400}>
-					<AppSidebar />
-				</Allotment.Pane>
-				<Allotment.Pane preferredSize={320} maxSize={400}>
-					<NoteList />
-				</Allotment.Pane>
-				<NoteEditor />
-			</Allotment>
+			<DragDropContext onDragEnd={onDragEnd}>
+				<Allotment minSize={200} proportionalLayout>
+					<Allotment.Pane preferredSize={240} maxSize={400}>
+						<AppSidebar />
+					</Allotment.Pane>
+					<Allotment.Pane preferredSize={320} maxSize={400}>
+						<NoteList />
+					</Allotment.Pane>
+					<NoteEditor />
+				</Allotment>
+			</DragDropContext>
 		</motion.section>
 	);
 };
