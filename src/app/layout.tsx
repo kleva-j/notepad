569ceb2d7c8e751f9910ec8d84e098223ed48468
kleva-j <kasmickleva@gmail.com>
2023-07-13 19:56:05 +0100

    feat: setup basic seo config and base font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,49 @@
 import React, { PropsWithChildren } from "react";
 
-import { SeoHeader } from "@/component/SeoHeader";
 import { getSession } from "@/lib/session";
+import { Roboto } from "next/font/google";
 import { headers } from "next/headers";
+import { Session } from "next-auth";
+import { Metadata } from "next";
 
 import Providers from "./provider";
 
 import "@/styles/globals.css";
-import { Session } from "next-auth";
+
+const title = "Notepad";
+const description = "This is a Note taking app.";
+
+export const metadata: Metadata = {
+	title,
+	description,
+	icons: {
+		icon: "/favicon.ico",
+		shortcut: "/favicon-16x16.png",
+		apple: "/apple-touch-icon.png",
+	},
+};
+
+const fontMono = Roboto({
+	weight: ["100", "300", "400", "500", "700", "900"],
+	subsets: ["latin"],
+});
 
 export default async function RootLayout({ children }: PropsWithChildren) {
 	const session = (await getSession(headers().get("cookie") ?? "")) as Session;
 
 	return (
 		<html lang="en">
-			<SeoHeader />
-			<body>
+			<meta
+				name="theme-color"
+				content="#000"
+				media="(prefers-color-scheme: dark)"
+			/>
+			<meta
+				name="theme-color"
+				content="#fff"
+				media="(prefers-color-scheme: light)"
+			/>
+			<body className={fontMono.className}>
 				<Providers session={session}>{children}</Providers>
 			</body>
 		</html>
