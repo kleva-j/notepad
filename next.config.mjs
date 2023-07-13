0c4678d6f55baa4a7a111d05040230035481908f
kleva-j <kasmickleva@gmail.com>
2023-07-13 19:56:43 +0100

    feat: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -6,19 +6,6 @@
 
 /** @type {import("next").NextConfig} */
 const config = {
-	reactStrictMode: true,
-
-	/**
-	 * If you have the "experimental: { appDir: true }" setting enabled, then you
-	 * must comment the below `i18n` config out.
-	 *
-	 * @see https://github.com/vercel/next.js/issues/41980
-	 */
-	// i18n: {
-	// 	locales: ["en"],
-	// 	defaultLocale: "en",
-	// },
-	experimental: { appDir: true, typedRoutes: true },
 	images: {
 		domains: ["avatars.githubusercontent.com"],
 	},
