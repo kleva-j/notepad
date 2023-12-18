f55cd2a5419b5fdf9d9d86f9fe000cb79c627fef
kleva-j <kasmickleva@gmail.com>
2023-12-18 16:14:47 +0100

    feat: project revamp of the trpc server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,13 @@
 "use client";
 
-import { api } from "@/utils/api";
-
 import React from "react";
 
-function RootPage() {
-	const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
+export default function RootPage() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
 			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-				<p className="text-2xl text-white">
-					{hello.data ? hello.data.greeting : "Loading tRPC query..."}
-				</p>
+				<p className="text-2xl text-white">Loading tRPC query</p>
 			</div>
 		</main>
 	);
 }
-
-export default api.withTRPC(RootPage);
