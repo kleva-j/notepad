1a05c0cc4b2736ec9828550dec16fdd9603eb9c5
kleva-j <kasmickleva@gmail.com>
2023-12-27 18:39:35 +0100

    fix: remove redundant eslint rules

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { atomWithStorage } from "jotai/utils";
 import { atom } from "jotai";
 
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/ban-types */
-
 import { ContextMenuEnum } from "@/utils/enums";
 import { twMerge } from "tailwind-merge";
 import { ClassValue, clsx } from "clsx";
