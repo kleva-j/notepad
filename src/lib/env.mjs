79e17d65a341bcbc0fe41456b39f1e40fd29bb7c
kleva-j <kasmickleva@gmail.com>
2024-01-23 00:26:39 +0100

    feat: create crud function for repo actions using plugins

--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -1,5 +1,6 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
+
 import "dotenv/config";
 
 export const env = createEnv({
@@ -8,15 +9,14 @@ export const env = createEnv({
       .enum(["development", "test", "production"])
       .default("development"),
     DATABASE_URL: z.string().min(1),
+		GITHUB_TOKEN: z.string().min(3),
+		REPO_NAME: z.string().min(3),
+		FILENAME: z.string().min(3),
+		FILEPATH: z.string().min(3),
   },
   client: {
     // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
   },
-  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
-  // runtimeEnv: {
-  //   DATABASE_URL: process.env.DATABASE_URL,
-  //   NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
-  // },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   experimental__runtimeEnv: {
     // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
