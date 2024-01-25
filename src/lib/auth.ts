27da19f58a4e22034e42471da9e59896ed6dda68
kleva-j <kasmickleva@gmail.com>
2024-01-25 16:36:31 +0100

    feat: setup github oauth using authjs

---
+++ src/lib/auth.ts
@@ -0,0 +1,10 @@
+import GitHub from "next-auth/providers/github";
+import NextAuth from "next-auth";
+
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/lib/db";
+
+export const { handlers, auth } = NextAuth({
+	providers: [GitHub],
+	adapter: DrizzleAdapter(db),
+});
--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -1,24 +1,20 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-import "dotenv/config";
-
 export const env = createEnv({
-  server: {
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
-    DATABASE_URL: z.string().min(1),
+	server: {
+		NODE_ENV: z
+			.enum(["development", "test", "production"])
+			.default("development"),
+		DATABASE_URL: z.string().min(1),
 		GITHUB_TOKEN: z.string().min(3),
 		REPO_NAME: z.string().min(3),
 		FILENAME: z.string().min(3),
 		FILEPATH: z.string().min(3),
-  },
-  client: {
-    // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
-  },
-  // For Next.js >= 13.4.4, you only need to destructure client variables:
-  experimental__runtimeEnv: {
-    // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
-  },
+		AUTH_GITHUB_ID: z.string().min(10),
+		AUTH_GITHUB_SECRET: z.string().min(10),
+		AUTH_SECRET: z.string().min(3),
+	},
+	client: {},
+	experimental__runtimeEnv: {},
 });
