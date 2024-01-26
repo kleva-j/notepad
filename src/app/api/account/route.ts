ce491d56ba09cb4f2fd983ce0a05b06778d94eb1
kleva-j <kasmickleva@gmail.com>
2024-01-26 15:46:17 +0100

    feat: implement update user api endpoint

---
+++ src/app/api/account/route.ts
@@ -0,0 +1,23 @@
+import { users } from "@/lib/db/schema/auth";
+import { revalidatePath } from "next/cache";
+import { AuthSession } from "@/types";
+import { db } from "@/lib/db/index";
+import { auth } from "@/lib/auth";
+import { eq } from "drizzle-orm";
+
+export async function PUT(request: Request) {
+	const session = await auth() as AuthSession;
+
+	if (!session) return new Response("Error", { status: 400 });
+
+	const body = (await request.json()) as { name?: string; email?: string };
+
+	await db
+		.update(users)
+		.set({ ...body })
+		.where(eq(users.id, session.user.id));
+
+	revalidatePath("/account");
+
+	return new Response(JSON.stringify({ message: "ok" }), { status: 200 });
+}
