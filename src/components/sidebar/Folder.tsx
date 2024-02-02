83751a8f3be2916bd7640b89fe868bff588c20b5
kleva-j <kasmickleva@gmail.com>
2024-02-02 17:58:12 +0100

    feat: style folderoptions component active and hover state

--- src/components/sidebar/Folder.tsx
+++ src/components/sidebar/Folder.tsx
@@ -10,37 +10,45 @@ export type FolderProps = {
 	icon: IconType;
 	active: boolean;
 	folder: Folder;
-	iconSize?: number;
 	onClick?: () => void;
 	addNoteType?: (noteId: string) => void;
 };
 
 export const FolderOption: FC<FolderProps> = (props) => {
-	const { text, icon, active, onClick, iconSize = 15 } = props;
+	const { text, icon, active, onClick } = props;
 	const Icon = motion(icon);
 
 	return (
 		<div
 			className={cn(
-				"group relative flex w-full cursor-pointer items-center gap-x-3 px-4 py-[10px] transition focus-visible:outline-1 focus-visible:outline-neutral-700",
+				"group relative flex cursor-pointer items-center gap-x-3 px-4 py-[10px] transition focus-visible:outline-[0.5px] focus-visible:outline-border",
 			)}
 			style={{ WebkitTapHighlightColor: "transparent" }}
 			onClick={onClick}
-			tabIndex={0}
+			draggable={true}
 		>
 			{active && (
 				<motion.span
 					layoutId="background"
-					className="absolute inset-0 z-10 bg-secondary mix-blend-difference dark:border-r-2 dark:border-r-sky-500"
+					className="absolute inset-0 z-10 border-r-2 border-r-blue-500 bg-secondary mix-blend-darken dark:mix-blend-difference"
 					transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
 				/>
 			)}
 			<motion.span
 				className={cn(
-					"flex items-center gap-x-1 text-[15px] font-medium leading-[1.4rem] tracking-[0.04em] text-white/90 transition-colors",
+					"flex items-center gap-x-1 text-[15px] font-semibold leading-[1.4rem] tracking-[0.04em] transition-colors",
+					active
+						? "text-foreground"
+						: "text-foreground/75 dark:text-foreground",
 				)}
 			>
-				<Icon size={iconSize} className={cn("mr-2 text-sky-500")} />
+				<Icon
+					size={15}
+					className={cn(
+						"mr-2 dark:text-blue-500",
+						active ? "text-blue-600" : "text-blue-500/90",
+					)}
+				/>
 				{text}
 			</motion.span>
 		</div>
