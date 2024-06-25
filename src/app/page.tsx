b09ea5124894101f11df85d239765a828eddad07
kleva-j <kasmickleva@gmail.com>
2024-06-25 09:05:41 +0100

    feat: temp landing page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Header from "@/layout/header";
+import Footer from "@/layout/footer";
+
 export default function RootPage() {
 	return (
-		<section className="flex h-screen w-screen">
-			Notepad Web Application
+		<section className="flex w-screen flex-col">
+			<div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
+			<Header variant="centered" sticky />
+			<Footer />
 		</section>
 	);
 }
