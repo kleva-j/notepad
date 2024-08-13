2cb4ee9e4fdd29c7aeecf922742694a6b1f49f27
Michael Obasi <kasmickleva@gmail.com>
2024-08-13 06:12:39 +0100

    feat: remove unused components, functions and packages (#65)
    The PR removes several components, files, and dependencies related to a previous markdown editing functionality. The `.gitignore`, `package.json`, and `pnpm-lock.yaml` files have been updated to reflect the removed files and dependencies. This cleanup helps in maintaining the project by removing unused or deprecated code.
    
    ### Browser checklist
    
    This PR has been tested in the following browsers:
    
    - [ ] Chrome
    - [ ] Firefox
    - [ ] Safari
    
    ### Testing checklist
    
    - [ ] End-to-end tests have been created if necessary

--- src/components/editor/index.ts
+++ src/components/editor/index.ts
@@ -1,10 +1,4 @@
-export { useProcessor } from "./markdown-processor";
-export { ReactMarkdown } from "./react-markdown";
 export { EmptyEditor } from "./EmptyEditor";
 export { EditorWrapper } from "./Wrapper";
-export { EditorMenubar } from "./menubar";
 export { Mention } from "./mention";
 export { people } from "./data";
-export { Code } from "./code";
-
-export * from "./utils";
