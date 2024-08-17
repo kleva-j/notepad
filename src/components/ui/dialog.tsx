428683f8e7481d511c64b9e251ca0978f033a843
kleva-j <kasmickleva@gmail.com>
2024-08-17 06:51:35 +0100

    feat: add form and textarea components, modify dialog component

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -16,13 +16,16 @@ const DialogClose = DialogPrimitive.Close;
 
 const DialogOverlay = forwardRef<
 	React.ElementRef<typeof DialogPrimitive.Overlay>,
-	React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
->(({ className, ...props }, ref) => (
+	React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay> & {
+		showOVerlay?: boolean;
+	}
+>(({ className, showOVerlay = true, ...props }, ref) => (
 	<DialogPrimitive.Overlay
 		ref={ref}
 		className={cn(
-			"fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+			"fixed inset-0 z-50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
 			className,
+			{ "bg-black/30": showOVerlay },
 		)}
 		{...props}
 	/>
@@ -31,10 +34,12 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 
 const DialogContent = forwardRef<
 	React.ElementRef<typeof DialogPrimitive.Content>,
-	React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, ...props }, ref) => (
+	React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & {
+		showOverlay?: boolean;
+	}
+>(({ className, showOverlay, children, ...props }, ref) => (
 	<DialogPortal>
-		<DialogOverlay />
+		<DialogOverlay showOVerlay={showOverlay} />
 		<DialogPrimitive.Content
 			ref={ref}
 			className={cn(
